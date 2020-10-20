1f2262897b356a0098b7efc64b8efb7747c1be7b
Santosh Shedbalkar <sanoysyg@gmail.com>
2020-10-21 03:36:52 +0530

    Added comment to a function

--- src/include/memory.h
+++ src/include/memory.h
@@ -5,6 +5,14 @@
 
 namespace merci {
     namespace memory {
+        /**
+         * @brief Call this function to get a stacktrace of the point where it
+         is being called from
+
+         Currently only works on Linux. Windows systems require .pdb files to
+         load symbol information
+         *
+         */
         EXPORTED extern pFnStrVoid getStackTrace;
-    }
+    }    // namespace memory
 }    // namespace merci
